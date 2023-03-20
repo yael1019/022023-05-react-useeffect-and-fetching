0abc7ab8a87cee1e4baa2c211cbe9e1b56cb5f79
Yael Harosh <yaelharosh01@gmail.com>
2023-03-20 16:42:47 -0400

    did post request

--- src/components/RaccoonsList.js
+++ src/components/RaccoonsList.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import RaccoonCard from './RaccoonCard'
 import RaccoonForm from "./RaccoonForm"
 
 
-// we are going to delete the raccoonsData in just a second.... //
-const raccoonsData = [
-  { id: 1, name: "Robert", favFood: "hamburgers" },
-  { id: 2, name: "Rocket", favFood: "pizza" },
-  { id: 3, name: "Rocky", favFood: "pizza" },
-  { id: 4, name: "Red", favFood: "hamburgers" },
-]
-// delete the above ^^^ //
-
-
 function RaccoonsList() {
 // SET STATE FOR THE RACCOON ARRAY THAT WE WILL CHANGE
-  const [raccoonArr, setRaccoonArr] = useState(raccoonsData)
+// start with empty arrary so it doesn't break our code
+  const [raccoonArr, setRaccoonArr] = useState([])
+// FETCHING USING USE EFFECT
+  useEffect(() => {
+    console.log('hello');
+    fetch('http://localhost:3002/raccoons')
+      .then(response => response.json())
+      .then(data => setRaccoonArr(data))
+
+    return () => console.log('cleaning up!');
+  }, [])
+      // the empty array is a dependency array --> this will fire only once (when it MOUNTS)
 // FUNCTION TO RESET RACCOONARR
   function addRaccoon(newRaccoonObj) {
-    setRaccoonArr(raccoonArr => [...raccoonArr, newRaccoonObj])
+    fetch('http://localhost:3002/raccoons', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify(newRaccoonObj)
+    })
+      .then(resp => resp.json())
+      // raccoon from DB has an ID which is very important for key and for things like a DELETE request
+      .then(newRaccoonFromDb => setRaccoonArr(raccoonArr => [...raccoonArr, newRaccoonFromDb]))
+    // setRaccoonArr(raccoonArr => [...raccoonArr, newRaccoonObj])
   }
 // MAPING THROUGH OUR ARRAY TO CREATE EACH RACCOON CARD
   const mappedRaccoons = raccoonArr.map(raccoon => <RaccoonCard key={ raccoon.id } raccoon={ raccoon }/>)
